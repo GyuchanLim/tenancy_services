4ca2ceeaa24c9d4e2881eaa5d0e9e3c5e64bfa7f
GyuchanLim <gyuchantech@gmail.com>
2025-02-16 16:51:46 +1300

    Refactor - remove unncessary service
    Call client directly when making calls from api controller.

--- app/models/api/tenancy/client.rb
+++ app/models/api/tenancy/client.rb
@@ -1,12 +1,16 @@
 module Api
-  module NewZealand
+  module Tenancy
     class Client
       include HTTParty
       base_uri "https://api.business.govt.nz/#{ENV["API_ENVIRONMENT"]}"
 
-      def initialize(uri, auth)
-        @uri = uri
-        @auth = auth
+      def initialize
+        @uri = ENV["TENANCY_MARKET_RENT_URI"]
+        @auth = {
+          "Content-Type" => "application/json",
+          "Cache-Control" => "no-cache",
+          "Ocp-Apim-Subscription-Key": ENV["TENANCY_MARKET_RENT_PRIMARY_KEY"]
+        }
       end
 
       def area_definitions(area = nil)
