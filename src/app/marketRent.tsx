5f5536b5674b6a75334af6456559ae333b2a40a0
GyuchanLim <gyuchantech@gmail.com>
2025-02-16 16:40:26 +1300

    feat - different selection boxes

--- src/app/marketRent.tsx
+++ src/app/marketRent.tsx
@@ -1,4 +1,6 @@
 import Container from '@components/layout/container'
+import RadioButton from '@components/common/radioButtons'
+import CheckBox from '@components/common/checkBoxes'
 import { callTenancyRequestRent } from '@hooks/callTenancyRequestRent'
 
 function MarketRent() {
@@ -16,6 +18,12 @@ function MarketRent() {
 					<p className="text-xl">Data Loaded</p> 
 					// <pre>{JSON.stringify(data, null, 2)}</pre>
 				}
+				<RadioButton radioButtonOptions={['Huey', 'Dewey', 'Louie', 'Donald', 'Mickey']} />
+				<CheckBox radioButtonOptions={['Huey', 'Dewey', 'Louie', 'Donald', 'Mickey']} />
+				{/* Area interested in? */}
+				{/* Property type? */}
+				{/* Information to know? (optional) */}
+				{/* display result */}
 		</Container>
 	)
 }
