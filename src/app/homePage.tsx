448da7fe2ae92769490469fae95a1231089287d6
GyuchanLim <gyuchantech@gmail.com>
2025-02-20 19:30:25 +1300

    New frontend routing

---
+++ src/app/homePage.tsx
@@ -0,0 +1,12 @@
+import Container from '@components/layout/container'
+
+function HomePage() {
+
+	return (
+		<Container>
+      
+		</Container>
+	)
+}
+
+export default HomePage
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Routes, Route } from "react-router";
 import Footer from '@components/layout/footer'
-import MarketRent from './marketRent.tsx'
+import TenancyPage from './tenancy/tenancyPage'
+import HomePage from './homePage'
 import '@stylesheets/main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
-      <MarketRent />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/tenancy" element={<TenancyPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
+
+    <Footer />
   </StrictMode>,
 )
