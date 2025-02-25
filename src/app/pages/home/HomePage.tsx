d13f6df36aed7d9d543a52912f984d43217d1c3c
GyuchanLim <gyuchantech@gmail.com>
2025-02-25 17:55:06 +1300

    fix indentations

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -2,16 +2,14 @@ import { Container } from "@components/layout"
 
 function HomePage() {
 
-	return (
-		<Container>
-			<div>
-				<h1>Introduction</h1>
-				<p>
-					This service provides users with the ability to customize their search on the market rent data of New Zealand using the public API provided by the Ministry of Business, Innovation, and Employment of New Zealand.
-				</p>
-			</div>
-		</Container>
-	)
+  return (
+    <Container>
+      <div>
+        <h1>Introduction</h1>
+        <p>This service provides users with the ability to customize their search on the market rent data of New Zealand using the public API provided by the Ministry of Business, Innovation, and Employment of New Zealand.</p>
+      </div>
+    </Container>
+  )
 }
 
 export default HomePage
