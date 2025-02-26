27395fc5f2acdf42f9177a891a357d64afa372e6
GyuchanLim <gyuchantech@gmail.com>
2025-02-26 23:25:33 +1300

    remove useless calls to API

--- src/app/pages/market_rent/MarketRent.tsx
+++ src/app/pages/market_rent/MarketRent.tsx
@@ -1,22 +1,9 @@
 import { Container } from '@components/layout'
-import useCallTenancySuburb from '@hooks/useCallTenancySuburb'
 import MarketRentSearchForm from "@components/MarketRentSearchForm";
 
 function MarketRent() {
-	const { data, error, isLoading } = useCallTenancySuburb('auckland')
-
 	return (
 		<Container>
-      {isLoading && 
-        <p className="text-xl">Loading...</p>
-      }
-      {error && 
-        <p className="text-xl">Error: {error.message}</p>
-      }
-      {data &&
-        <p className="text-xl">Data Loaded</p> 
-        // <pre>{JSON.stringify(data, null, 2)}</pre>
-      }
       <h1>Market Rent</h1>
       <MarketRentSearchForm />
 		</Container>
