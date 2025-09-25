062625a361f0327dd65e4a8748a88e842c269fe9
Gyuchan Lim <gyuchantech@gmail.com>
2025-09-25 18:13:31 +1200

    Finalize data structure of our Tenancy Application
    This commit allows us to save BondStatistics locally and fetch without
    having to make API calls for each months.
    
    To do
    - Add tests.

--- app/models/tenancy_services/area_label.rb
+++ app/models/tenancy_services/area_label.rb
@@ -2,6 +2,7 @@ module TenancyServices
   # Area Labels Table
   class AreaLabel < ApplicationRecord
     belongs_to :area_definition, class_name: "TenancyServices::AreaDefinition"
+    has_many :bond_statistics, class_name: "TenancyServices::BondStatistics"
 
     validates :label, presence: true
     validates :code, presence: true
